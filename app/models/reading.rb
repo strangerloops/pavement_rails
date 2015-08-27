e7d3c76112661d620ff87372af1aba101f050ae4
strangerloops <hassinmichael@gmail.com>
2015-08-27 00:40:38 -0500

    hh

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,10 +1,10 @@
 class Reading < ActiveRecord::Base
 	def packet
-		{
-			start_location: [start_lat,start_lon],
-			end_location: [end_lat,end_lon],
-			roughness: get_roughness
-		}.to_json
+		"{
+			start_location: #{[start_lat,start_lon]},
+			end_location: #{[end_lat,end_lon]},
+			roughness: #{get_roughness}
+		}"
 	end
 
 	def get_roughness
