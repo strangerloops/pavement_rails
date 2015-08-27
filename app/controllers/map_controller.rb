dda76c76c5f0514c49061a8978dd14a2c08cb327
strangerloops <hassinmichael@gmail.com>
2015-08-26 21:02:37 -0500

    gy

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,14 @@
 class MapController < ApplicationController
   def home
-  	@foo = "hey"
+
+  	@accels = Reading.all.map do |reading|
+  		reading.acceleration
+  	end.reject do |accel|
+  		accel.empty?
+  	end.map do |non_empty_accel|
+  		non_empty_accel[2..-1].split.map do |val|
+  			val.to_f
+  		end
+  	end
   end
 end
