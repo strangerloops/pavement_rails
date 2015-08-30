cbc2cfef50e96245769ade2b68b020d3464302b6
strangerloops <hassinmichael@gmail.com>
2015-08-30 14:25:36 -0500

    scratch that

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,6 @@
 class RidesController < ApplicationController
 
 	def create
-		if ride_params['calibration_id'].empty? then ride_params['calibration_id'] = ride_params['id'] end
 		@ride = Ride.new(ride_params)
 		respond_to do |format|
 			if @ride.save
