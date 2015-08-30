87b468744e2007fc6a2699047a7d1abbb4bc028b
strangerloops <hassinmichael@gmail.com>
2015-08-30 14:19:31 -0500

    calibration method etc

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,6 @@
 class Ride < ActiveRecord::Base
-	has_one :calibration, :class_name => 'Ride', :foreign_key => 'id'
+	# has_one :calibration, :class_name => 'Ride', :foreign_key => 'id'
+	def calibration
+		Ride.find(calibration_id)
+	end
 end
