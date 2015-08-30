7c4148321834a420135349678a6e48a4fef80591
strangerloops <hassinmichael@gmail.com>
2015-08-30 13:10:20 -0500

    find readings by ride

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -1,6 +1,6 @@
 module MapHelper
 	def packets_for_ride ride
-		ride.readings.select do |reading|
+		Reading.where(ride_id: ride.id).select do |reading|
   		reading.has_accel?
   	end.map do |reading|
   		reading.packet
