352fa879805e158e8ace5292784af4cac9557d4f
strangerloops <hassinmichael@gmail.com>
2015-08-30 12:46:44 -0500

    hashrockets

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get 'static_pages/contribute'
   get 'static_pages/contact'
 
-  get 'map/all', 'map#all_rides'
-  get 'map/last', 'map#last_ride'
-  get 'map/ride/:id', 'map#packets_for_ride'
+  get 'map/all' => 'map#all_rides'
+  get 'map/last' => 'map#last_ride'
+  get 'map/ride/:id' => 'map#packets_for_ride'
 
   resources :readings, :rides
 
