6b76f8900442f136313533943fa9f76ecddcca11
strangerloops <hassinmichael@gmail.com>
2015-08-31 10:47:57 -0500

    remove map from static

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,6 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def map
-  end
-
   def contribute
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   
   get 'static_pages/home'
-  get 'static_pages/map'
   get 'static_pages/contribute'
   get 'static_pages/contact'
 
