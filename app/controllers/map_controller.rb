df6b81f85db3ab2feee10d7a7f94ae28d27b75be
strangerloops <hassinmichael@gmail.com>
2015-08-31 21:26:50 -0500

    adjust that mean

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -32,13 +32,13 @@ class MapController < ApplicationController
 
   def all_rides_adjusted_mean
     @packets = Ride.all.map do |ride|
-      mean_packets_for ride
+      adjusted_mean_packets_for ride
     end.reduce(&:+)
     render :map
   end
 
   def one_ride_adjusted_mean
-    @packets = mean_packets_for Ride.find(params[:id])
+    @packets = adjusted_mean_packets_for Ride.find(params[:id])
     render :map
   end
 end
