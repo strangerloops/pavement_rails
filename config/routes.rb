da814f1d23f55b0e2fea48af4d3ecc056e382c2e
strangerloops <hassinmichael@gmail.com>
2015-09-01 00:40:47 -0500

    home route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'map/all-mean' => 'map#all_rides_mean'
   get 'map/ride-mean/:id' => 'map#one_ride_mean'
 
+	get '/' => 'map#all_rides_adjusted_mean'
   get 'map/all-adjusted' => 'map#all_rides_adjusted_mean'
   get 'map/ride-adjusted/:id' => 'map#one_ride_adjusted_mean'
 
