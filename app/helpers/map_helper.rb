4d3834e6c531bacc93ae5ff6d5173e0e30e3b4fa
strangerloops <hassinmichael@gmail.com>
2015-10-18 12:19:24 -0500

    unused code

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -5,26 +5,7 @@ module MapHelper
   	end
 	end
 
-	def mean_packets_for ride
-		(readings_for ride).map do |reading|
-  		reading.mean_packet
-  	end
-	end
-
-	def adjusted_mean_packets_for ride
-		(readings_for ride).map do |reading|
-  		reading.adjusted_mean_packet
-  	end
-	end
-
 	def readings_for ride
 		Reading.where(ride_id: ride.id)
 	end
-
-	def mean_packets_with_speed_for ride
-		(readings_for ride).map do |reading|
-  		reading.mean_packet_plus_speed
-  	end
-	end
-
 end
