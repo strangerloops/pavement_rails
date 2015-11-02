fdd2e998ee1256bca2d0a29dda5e9ce515dec82b
strangerloops <hassinmichael@gmail.com>
2015-11-02 11:12:13 -0600

    rufus

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -9,10 +9,11 @@ keep_alive_scheduler.every five_seconds, :first_in => 0.1 do
 	# ping '/'
 end
 
-# zip_scheduler.every day, :first_in => 0.1 do
-# 	# zip it all up
-# 	readings = Reading.all.to_json
-# 	File.open(File.join(Rails.root, 'cache', 'readings.txt'), 'w+') do |f|
-# 		f.write readings
-# 	end
-# end
+zip_scheduler.every day, :first_in => 160.0 do
+	# zip it all up
+	p 'writing cache...'
+	readings = Reading.all.to_json
+	File.open(File.join(Rails.root, 'cache', 'readings.txt'), 'w+') do |f|
+		f.write readings
+	end
+end
