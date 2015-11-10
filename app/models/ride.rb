6fbc48b51244f31f0f3cc5b8830be14a359833f5
strangerloops <hassinmichael@gmail.com>
2015-11-09 18:00:32 -0600

    add trim to ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,12 @@
 class Ride < ActiveRecord::Base
 	has_many :readings, dependent: :destroy
+
+	def trim
+		upper_bound = readings.count / 10
+		front = readings[0..rand(upper_bound)]
+		back = readings[(readings.count - rand(upper_bound))..-1]
+		(front + back).each do |reading|
+			reading.destroy
+		end
+	end
 end
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 
 keep_alive_scheduler = Rufus::Scheduler.new
-zip_scheduler = Rufus::Scheduler.new
+daily_scheduler = Rufus::Scheduler.new
 
 day = "#{60 * 60 * 24}s"
 four_minutes = "#{4 * 60}s"
@@ -11,19 +11,53 @@ keep_alive_scheduler.every four_minutes, :first_in => 1.0 do
   p Time.now.inspect
 end
 
-zip_scheduler.every day, :first_in => 12.0 do
-	
-	p 'writing cache...'
+daily_scheduler.every day, :first_in => 12.0 do
+
+	p 'caching readings...'
+	cache_readings
+	p 'done with readings.'
+
+	p 'caching mean packets...'
+	cache_mean_packets
+	p 'done with packets.'
+
+	p 'caching scoreboard...'
+	cache_scoreboard
+	p 'done with scoreboard.'
+end
+
+def cache_readings
 	readings = Reading.all.to_json
 	File.open(File.join(Rails.root, 'cache', 'readings.txt'), 'w+') do |f|
 		f.write readings
 	end
-	p 'done with readings.'
+end
 
-	p 'writing mean packets...'
+def cache_mean_packets
 	packets = Reading.all.map do |r| r.mean_packet end.to_json
 	File.open(File.join(Rails.root, 'cache', 'mean_packets.txt'), 'w+') do |f|
 		f.write packets
 	end
-	p 'done with packets.'
 end
+
+def cache_scoreboard
+	# sort an array of all device IDs by 
+	# put the scoreboard in the DB? how else to serialize it?
+
+	# pluck here?
+	highest_id = Ride.all.map do |ride| ride.device_id end.max
+	1.upto highest_id do |id|
+		Ride.find(id).distance
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
