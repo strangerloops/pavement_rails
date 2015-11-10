4e12e36affeabb6e54ca1ca4d79c313bd014084b
strangerloops <hassinmichael@gmail.com>
2015-11-09 20:02:24 -0600

    sort before trim

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,9 +2,10 @@ class Ride < ActiveRecord::Base
 	has_many :readings, dependent: :destroy
 
 	def trim
-		upper_bound = readings.count / 10
-		front = readings[0..rand(upper_bound)]
-		back = readings[(readings.count - rand(upper_bound))..-1]
+		sorted_readings = readings.sort { |i, j| i.start_time <=> j.start_time }
+		upper_bound = sorted_readings.count / 10
+		front = sorted_readings[0..rand(upper_bound)]
+		back = sorted_readings[(sorted_readings.count - rand(upper_bound))..-1]
 		(front + back).each do |reading|
 			reading.destroy
 		end
