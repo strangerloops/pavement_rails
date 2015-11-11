6cc535d7618e8d5d663bc1d1f0e9d5b6915b4cc7
strangerloops <hassinmichael@gmail.com>
2015-11-11 14:37:21 -0600

    scoreboard controller

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,6 +1,6 @@
 class ScoreboardsController < ApplicationController
 
 	def rank
-		render :plain (Scoreboard.where(description: 'global').first.index(params[:id]) || -1) + 1
+		render plain: ((Scoreboard.where(description: 'global').first.index(params[:id]) || -1) + 1)
 	end
 end
