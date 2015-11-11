d696b9e78776b4ed860a7ac968ebcb8d9e01ac33
strangerloops <hassinmichael@gmail.com>
2015-11-11 16:18:49 -0600

    render json

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,6 +1,6 @@
 class ScoreboardsController < ApplicationController
 
 	def rank
-		render plain: ((Scoreboard.where(description: 'global').first.rankings.index(params[:id].to_i) || -1) + 1)
+		render json: ((Scoreboard.where(description: 'global').first.rankings.index(params[:id].to_i) || -1) + 1).to_json
 	end
 end
