e29045b8bb5f5de8afd01ac48813c83422e227d4
strangerloops <hassinmichael@gmail.com>
2015-11-11 14:59:46 -0600

    cast id param to integer

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,6 +1,6 @@
 class ScoreboardsController < ApplicationController
 
 	def rank
-		render plain: ((Scoreboard.where(description: 'global').first.rankings.index(params[:id]) || -1) + 1)
+		render plain: ((Scoreboard.where(description: 'global').first.rankings.index(params[:id].to_i) || -1) + 1)
 	end
 end
